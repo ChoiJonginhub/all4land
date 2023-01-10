28d9df7c92ee1871971163c82d01600bc5f8d8a5
cj6552 <98302080+ChoiJonginhub@users.noreply.github.com>
2023-01-10 12:29:31 +0900

    Update MyStack.java
    underflow 추가

--- MyStack.java
+++ MyStack.java
@@ -1,12 +1,13 @@
 package memory;
 
 public class MyStack extends Memory {
-	public void push(int n) {
-		data[count++]=n;
-	}
 	public void pop() {
-		count-=1;
-		data[count]=0;
+		if(count-1<0) {
+			System.out.println("Underflow!");
+		}else {
+			count--;
+			data[count]=0;			
+		}
 	}
 	public void show() {
 		for(int i=0; i<5; i++) {
